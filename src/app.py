324246dff2cc32d0fd78bb380a9acf7c99069294
angelaro93 <angela.ro.93@gmail.com>
2024-10-26 12:12:30 +0000

    ver cambios

--- src/app.py
+++ src/app.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Planet, Fav
 #from models import Person
 
 app = Flask(__name__)
@@ -37,13 +37,64 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_all_users():
+    users = User.query.all()  # Obtener todos los registros de User
+    users_list = [user.serialize() for user in users]  # Serializar cada registro
+    return jsonify(users_list), 200
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+@app.route('/user/<int:id>/favs', methods=['GET'])
+def get_users_favs(id):
+    user = User.query.get(id)  # Obtener todos los registros de User
+    favs = [fav.serialize() for fav in user.favs]  # Serializar cada registro
+    return jsonify(favs), 200
 
-    return jsonify(response_body), 200
+
+@app.route('/character', methods=['GET'])
+def get_all_characters():
+    characters = Character.query.all()  
+    characters_list = [character.serialize() for character in characters]  
+    return jsonify(characters_list), 200
+
+@app.route('/character/<int:character_id>', methods=['GET'])
+def get_character(character_id):
+    character = Character.query.get(character_id)
+    if character is None:
+        raise APIException('Character not found', status_code=404)
+    return jsonify(character.serialize()), 200
+
+
+
+
+@app.route('/planets', methods=['GET'])
+def get_all_planets():
+    planets = Planet.query.all()  
+    planets_list = [planet.serialize() for planet in planets]  
+    return jsonify(planets_list), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    planet = Planet.query.get(planet_id)
+    if planet is None:
+        raise APIException('Planet not found', status_code=404)
+    return jsonify(planet.serialize()), 200
+
+@app.route('/favorite/planet', methods=['POST'])
+def add_favorite_planet(planet_id):
+    user_id= request.json.get(user_id)
+    planet_id = request.json.get(planet_id)
+
+    user = User.query.get(user_id)
+    id_user = user.id
+    existing_fav = Fav.query.filter_by(id_user = user_id, id_planet = planet_id).first()
+    if existing_fav: 
+        return jsonify({"Error": "Planet already in favorites"}), 400
+    
+    new_fav = Fav(id_user = user_id, id_planet = planet_id)
+
+    db.session.add(new_fav)
+    db.session.commit()
+
+    return jsonify(new_fav.serialize()), 201 
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
